4562dcadf8af2d325d6bc32f18c15717553689b0
bin <newxiaobin@gmail.com>
2023-04-06 17:29:59 +0800

    public success

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -3,77 +3,66 @@ import Head from "next/head";
 import Image from "next/image";
 import { getFreelistProof } from "../../hardhat/scripts/getRoot";
 import CountDown from "../components/CountDown";
-import abi from "../contract/abi.json";
-// import { useScaffoldContractWrite } from "../hooks/scaffold-eth/useScaffoldContractWrite";
+import { useScaffoldContractRead } from "../hooks/scaffold-eth/useScaffoldContractRead";
+import { useScaffoldContractWrite } from "../hooks/scaffold-eth/useScaffoldContractWrite";
 import dog from "./dog.jpg";
 import pic from "./pic.png";
-import { ethers } from "ethers";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { localhost } from "wagmi/chains";
-import { getLocalProvider } from "~~/utils/scaffold-eth";
-
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const provider = getLocalProvider(localhost);
-const contract = new ethers.Contract(contractAddress, abi, provider);
-let signer;
-let contractWithSigner: any;
-if (provider) {
-  signer = provider.getSigner();
-  contractWithSigner = contract.connect(signer);
-}
-console.log(contract);
 
 const Home: NextPage = () => {
-  const [startTime, setstartTime] = useState<number>(0);
-  const [endTime, setendTime] = useState<number>(0);
-  const [nowTime, setnowTime] = useState<number>(0);
-  const [maxSupply, setmaxSupply] = useState<number>(0);
-  const [tokenId, settokenId] = useState<number>(0);
-  const [notRevealedUri, setnotRevealedUri] = useState<string>("");
-  const [baseURI, setbaseURI] = useState<string>("");
   const { address } = useAccount();
+
+  const nowTime = new Date().getTime();
+  const { data: startTime } = useScaffoldContractRead({
+    contractName: "SE2H",
+    functionName: "getMintStartTime",
+  });
+  const { data: endTime } = useScaffoldContractRead({
+    contractName: "SE2H",
+    functionName: "getMintEndTime",
+  });
+  const { data: baseURI } = useScaffoldContractRead({
+    contractName: "SE2H",
+    functionName: "getBaseUri",
+  });
+  const { data: tokenId } = useScaffoldContractRead({
+    contractName: "SE2H",
+    functionName: "getTokenId",
+  });
+  const { data: maxSupply } = useScaffoldContractRead({
+    contractName: "SE2H",
+    functionName: "getMaxSupply",
+  });
+  const { writeAsync: writePublic } = useScaffoldContractWrite({
+    contractName: "SE2H",
+    functionName: "publicMint",
+    value: "0.1",
+  });
+
+  const [_args, set_args] = useState<any>();
+  const { writeAsync: freelistMint } = useScaffoldContractWrite({
+    contractName: "SE2H",
+    functionName: "freelistMint",
+    args: _args,
+  });
 
   const getfinish = () => {
     console.log("finish");
   };
 
   const freeMint = () => {
-    const freeproof = getFreelistProof(address)[0];
-    const _freeproof = `["${freeproof}"]`;
-    const _args = JSON.parse(_freeproof);
-    console.log(_args);
-
-    contractWithSigner.freelistMint(_args).then((data: any) => {
-      console.log(data);
-    });
+    const freeproof = getFreelistProof(address);
+    set_args(freeproof);
+    console.log(_args, freeproof);
+    console.log(1, freelistMint());
+  };
+  const PublicMint = () => {
+    console.log(2, writePublic());
   };
 
   useEffect(() => {
-    contract.mintEndTime().then((data: number) => {
-      const endtime = parseInt(String(data), 10);
-      setendTime(endtime);
-    });
-    contract.mintStartTime().then((data: number) => {
-      const starttime = parseInt(String(data), 10);
-      setstartTime(starttime);
-    });
-    contract.i_maxSupply().then((data: number) => {
-      const _maxSupply = parseInt(String(data), 10);
-      setmaxSupply(_maxSupply);
-    });
-    contract.tokenId().then((data: number) => {
-      const _tokenId = parseInt(String(data), 10);
-      settokenId(_tokenId);
-    });
-    contract.notRevealedUri().then((data: string) => {
-      setnotRevealedUri(data);
-    });
-    contract.baseURI().then((data: string) => {
-      setbaseURI(data);
-    });
-
-    setnowTime(new Date().getTime());
+    console.log("start home");
   }, []);
 
   return (
@@ -90,7 +79,7 @@ const Home: NextPage = () => {
           <div className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
             <div className="text-center font-bold ">SUPPLY</div>
             <div className="rounded-full h-24 w-24 flex items-center justify-center border border-4 border-violet-800">
-              {tokenId}/{maxSupply}
+              {Number(tokenId)}/{Number(maxSupply)}
             </div>
           </div>
           {/* 1 */}
@@ -100,19 +89,13 @@ const Home: NextPage = () => {
               <span>{}</span>
             </div>
             <div className="flex flex-row justify-center">
-              {startTime !== 0 && startTime <= nowTime && endTime > nowTime ? (
+              {Number(startTime) !== 0 && Number(startTime) <= nowTime && Number(endTime) > nowTime ? (
                 <div style={{ color: "#ffbf70" }}>
-                  End in <CountDown max={endTime - nowTime} finish={getfinish} />
+                  End in <CountDown max={Number(endTime) - nowTime} finish={getfinish} />
                 </div>
               ) : null}
             </div>
-            <Image
-              src={baseURI ? dog : pic}
-              alt={notRevealedUri}
-              width={200}
-              priority
-              className="border border-2 border-black"
-            />
+            <Image src={baseURI ? dog : pic} alt="" width={200} priority className="border border-2 border-black" />
             <div
               className="btn btn-primary btn-sm rounded-full font-normal normal-case w-20 relative z-10 mt-4"
               onClick={freeMint}
@@ -127,17 +110,17 @@ const Home: NextPage = () => {
               <span></span>
             </div>
             <div>
-              {startTime !== 0 && startTime > nowTime ? (
+              {Number(startTime) !== 0 && Number(startTime) > nowTime ? (
                 "Pending"
-              ) : endTime > nowTime ? (
+              ) : Number(endTime) > nowTime ? (
                 <div>
-                  End in <CountDown max={endTime - nowTime} finish={getfinish} />
+                  End in <CountDown max={Number(endTime) - nowTime} finish={getfinish} />
                 </div>
               ) : (
                 "Closed"
               )}
             </div>
-            <Image src={baseURI ? dog : pic} alt={notRevealedUri} width={200} priority />
+            <Image src={baseURI ? dog : pic} alt="" width={200} priority />
             <div className="btn btn-primary btn-sm rounded-full font-normal normal-case w-20 relative z-10">FREE</div>
           </div>
           {/* 3 */}
@@ -153,8 +136,13 @@ const Home: NextPage = () => {
                 </div>
                 <div></div>
               </div>
-              <Image src={baseURI ? dog : pic} alt={notRevealedUri} width={200} priority />
-              <div className="btn btn-primary btn-sm rounded-full font-normal normal-case w-20 relative z-10">FREE</div>
+              <Image src={baseURI ? dog : pic} alt="" width={200} priority />
+              <div
+                className="btn btn-primary btn-sm rounded-full font-normal normal-case w-20 relative z-10"
+                onClick={PublicMint}
+              >
+                PublicMint
+              </div>
             </div>
           </div>
         </div>
